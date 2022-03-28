00b467a151e296fb8bbfd97b05610b6f66728872
Carl Norlen <cnorlen@uci.edu>
2022-03-28 13:59:18 -0700

    Added a biomass time series to figure 2 to add counter point to nothing left to kill criticism.

--- fig2_time_series.r
+++ fig2_time_series.r
@@ -47,6 +47,7 @@ time.series$tpa_p50 <- time.series$tpa_p50 * 2.41705
 time.series$tpa_p75 <- time.series$tpa_p75 * 2.41705 
 time.series$tpa_p25 <- time.series$tpa_p25 * 2.41705
 time.series$tpa_low_area <- (time.series$tpa_mid_sum / time.series$tpa_count) * 100 
+time.series$biomass_mean[time.series$biomass_mean == 0] <- NA
 
 #Make the drought/Region a factor
 time.series$drought <- factor(time.series$drought)
@@ -82,8 +83,25 @@ p2 <- ggplot() +
 	  axis.text.x=element_blank(), plot.margin = unit(c(0.5,0.5,0.5,0.5), "pt"), legend.title = element_text(size = 8), legend.text = element_text(size = 6)) +
 	  ylab('dNDMI') + geom_text(data = data.frame(label = "95% CI", x = as.Date('2018-06-30'), y = -0.019, sequence = 'Both Droughts'), mapping = aes(x=x, y=y, label = label), size = 1) 
 
-#Create a Pr-ET time series graph
+#Create a biomass density time series.
 p3 <- ggplot() +
+  geom_rect(data = data.frame(xmin = as.Date('1998-10-01'), xmax = as.Date('2002-09-30'), ymin = -Inf, ymax = Inf), 
+            fill = "red", alpha = 0.3, mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)) +
+  geom_rect(data = data.frame(xmin = as.Date('2011-10-01'), xmax = as.Date('2015-09-30'), ymin = -Inf, ymax = Inf), 
+            fill = "red", alpha = 0.3, mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)) +
+  geom_line(data = time.series, mapping = aes(x = time_start, y = biomass_mean, color = drought, linetype = drought), size = 0.5) +
+  geom_errorbar(data=time.series, mapping=aes(x=time_start, ymin=biomass_mean - (biomass_stdDev / sqrt(count) * 1.96), ymax=biomass_mean + (biomass_stdDev / sqrt(count) * 1.96), color = drought, linetype = drought), 
+                size = 0.4, width = 100, alpha = 0.6) +
+  theme_bw() + ylim(100, 190) +
+  geom_hline(yintercept = 0, linetype='dashed') + 
+  scale_linetype_manual(values=c("solid", "twodash")) +
+  scale_color_manual(values=c("#E66100", "#5D3A9B")) +
+  theme(axis.text.y = element_text(size = 8), axis.title.y = element_text(size = 10), axis.title.x = element_blank(), legend.position = 'none', 
+        axis.text.x = element_blank(), plot.margin = unit(c(0.5,0.5,0.5,0.5), "pt"), legend.title = element_text(size = 8), legend.text = element_text(size = 6)) + 
+  ylab(expression('Biomass\n(Mg ha'^-1*')')) 
+
+#Create a Pr-ET time series graph
+p4 <- ggplot() +
 	  geom_rect(data = data.frame(xmin = as.Date('1998-10-01'), xmax = as.Date('2002-09-30'), ymin = -Inf, ymax = Inf), 
 	  fill = "red", alpha = 0.3, mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax)) +
       geom_rect(data = data.frame(xmin = as.Date('2011-10-01'), xmax = as.Date('2015-09-30'), ymin = -Inf, ymax = Inf), 
@@ -100,7 +118,7 @@ p3 <- ggplot() +
 	  xlab('Year') + ylab(expression('Pr-ET\n(mm yr'^-1*')')) 
 
 #Combine the three time series graphs into one
-(p1 / p2 / p3) + plot_annotation(tag_levels = 'a', theme = theme(legend.margin=margin(t = 0, unit='cm'))) + plot_layout(heights = c(0.95,0.95,1))
+(p1 / p2 / p3 / p4) + plot_annotation(tag_levels = 'a', theme = theme(legend.margin=margin(t = 0, unit='cm'))) + plot_layout(heights = c(0.95,0.95,0.95,1))
 
 #Save and export the figure as a .png file
-ggsave(filename = 'Fig2_drought_time_series.png', height=11, width=11, units = 'cm', dpi=900)
+ggsave(filename = 'Fig2_drought_time_series.png', height=14, width=11, units = 'cm', dpi=900)
